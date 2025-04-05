32fa498bae3f4d5c065501bac1408b56170ae06d
alishoshi <ali.shoshi@student.uni-pr.edu>
2025-04-05 11:17:18 +0200

    riemerime

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
     <div class="header-content">
       <h1>Amanpuri Hotel</h1>
       <nav>
-        <a href="index.html" >Home</a>
+        <a href="index.php" >Home</a>
         <a href="rooms.php">Rooms</a>
-        <a href="restaurant.html">Restaurant</a>
-        <a href="about.html">About</a>
-        <a href="contact.html">Contact</a>
+        <a href="restaurant.php">Restaurant</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact</a>
         <a href="book.php" class="book">Book Now</a>
       </nav>
     </div>
