0b1a9636035ef349615d263f452d8390f967cfca
Erzana Beqaj <erzana.beqaj@student.uni-pr.edu>
2025-04-19 15:11:16 +0200

    Shtimi i materialit ne pjesen e restaurantit

--- restaurant.php
+++ restaurant.php
@@ -15,7 +15,14 @@
   <main >
     <div style="justify-content: center; text-align: center; border-radius: 15px; background-color: #ffde6561; margin-top: -50px;margin-bottom: -30px;">
         <h5 style="font-size: 2rem; color: #f8f8f8;">RESTAURANT</h5> <!-- Smaller font-size -->
-      </div>  </main>
+      </div> 
+      
+      <h2 style="color: #f5c518;justify-content: center; text-align: center;">Welcome to Amanpuri Restaurant</h2>
+      <p style=" font-size: 1.5rem ;padding :30px;justify-content: center; text-align: center; border-radius: 10px; background-color: #ffffff25;color:#f5c518;margin: 50px 0;">
+      Join us for a unique experience with a rich menu that includes fresh seafood, a variety of expertly prepared meats, handcrafted pizzas, flavorful Australian burgers, masterfully made Japanese sushi, and much more.
+      Each dish is crafted with passion to satisfy every palate – from the most traditional to the most adventurous. Our welcoming and modern atmosphere is perfect for a dinner with friends, a family lunch, or a special night out. 
+</p>
+    </main>
       
   
 
